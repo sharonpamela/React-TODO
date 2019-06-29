aa40f1214c343d5b8827bd0ea8c206d13b389013
sharonpamela <sharon.pamela.santana@gmail.com>
2019-06-29 11:13:45 -0700

    re-structured files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoItem from './components/TodoItem';
+
+function App() {
+  return (
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoItem() {
+
+    return (
+        <div className="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default TodoItem;
